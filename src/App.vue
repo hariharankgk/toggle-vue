6ffe15e65af936ad4b06377431fe815a91915d1d
Hariharan G (Photon) <hariharan.g@photon.in>
2022-11-25 14:55:14 +0530

    project init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="toggle">
+    <ToggleSwitch :checked="false" @updateInput="getValue" />
+    <ToggleSwitch :checked="true"  @updateInput="getValue" />
+    <ToggleSwitch :disabled="true" />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import ToggleSwitch from './components/ToggleSwitch.vue'
+  function getValue(val) {
+    console.log(val)
+  }
+</script>
+
+<style lang="scss">
+  :root {
+    --grey1: rgba(0, 0, 0, 0.1);
+    --grey2: rgba(0, 0, 0, 0.2);
+    --pink: #9747FF;
+    --green: #518D6C;
+    --white: #FFFFFF;
+  }
+  
+  
+  .toggle {
+    display: flex;
+    justify-content: space-around;
+    border:1px dashed var(--pink);
+    align-items: center;
+    border-radius: 5px;
+    padding: 10px;
+    width: 222px;
+    height: 56px;
+  }
+</style>
---
+++ src/components/ToggleSwitch.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <label class="toggle__switch">
+      <input type="checkbox" @input="onInput" :disabled="disabled" :checked="checked">
+      <span class="toggle__switch--slider toggle__switch--round"></span>
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    disabled: { type: Boolean, default: false },
+    checked: { type: Boolean, default: false }
+  })
+  const changeEmitName = 'updateInput';
+  const emits = defineEmits<{(e: typeof changeEmitName, id: string): void }>();
+  const onInput = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    emits(changeEmitName, target.checked);
+  };
+</script>
+
+<style lang="scss">
+.toggle__switch {
+  position: relative;
+  display: inline-block;
+  width: 34px;
+  height: 16px;
+
+  .toggle__switch--slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: var(--grey2);
+    -webkit-transition: .4s;
+    transition: .4s;
+
+    &:before {
+      position: absolute;
+      content: "";
+      height: 8px;
+      width: 8px;
+      left: 4px;
+      bottom: 4px;
+      background-color: var(--white);
+      -webkit-transition: .4s;
+      transition: .4s;
+    }
+
+    &.toggle__switch--round {
+      border-radius: 34px;
+      &:before {
+        border-radius: 50%;
+      }
+    }
+  }
+
+  input[type='checkbox'] { 
+    opacity: 0;
+    width: 0;
+    height: 0;
+
+    &:disabled~.toggle__switch--slider {
+      background-color: var(--grey1);
+    }
+
+    &:checked~.toggle__switch--slider {
+      background-color: var(--green);
+    }
+
+    &:focus~.toggle__switch--slider {
+      box-shadow: 0 0 1px var(--green);
+    }
+
+    &:checked + .toggle__switch--slider:before {
+      -webkit-transform: translateX(16px);
+      -ms-transform: translateX(16px);
+      transform: translateX(16px);
+      width: 12px;
+      height: 12px;
+      top: 2px;
+    }
+  }
+}
+</style>
